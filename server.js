c9f9c3f4eccfa942335ac06acf2b1ed5a0c7d82f
hoangsangnguyen <nguyenhoangsang6014@gmail.com>
2018-10-09 15:07:59 +0700

    no message

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { createServer } = require('http');
 const next = require('next');
-
+const { parse } = require('url')
 const app = next ({
     dev : process.env.NODE_ENV !== 'production'
 });
@@ -9,8 +9,21 @@ const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-    createServer(handler).listen(3000, err => {
-        if (err) throw err;
-        console.log('Ready on localhost : 3000');
-    });
+    createServer((req, res) => {
+        // Be sure to pass `true` as the second argument to `url.parse`.
+        // This tells it to parse the query portion of the URL.
+        const parsedUrl = parse(req.url, true)
+        const { pathname, query } = parsedUrl
+        console.log("========>", pathname, query)
+        if (pathname === '/campaigns') {
+          app.render(req, res, '/b', query)
+        } else if (pathname === '/b') {
+          app.render(req, res, '/a', query)
+        } else {
+            handler(req, res, parsedUrl)
+        }
+      }).listen(3000, err => {
+        if (err) throw err
+        console.log('> Ready on http://localhost:3000')
+      })
 });
